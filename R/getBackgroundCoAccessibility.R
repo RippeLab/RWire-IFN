131dac62fd211f003664a3d6a1101eb219a879c8
IsabelleLander <49637511+isaLa42@users.noreply.github.com>
2023-01-31 11:21:11 +0100

    Add files via upload

---
+++ R/getBackgroundCoAccessibility.R
@@ -0,0 +1,232 @@
+#' getBackgroundCoAccessibility
+#' 
+#' This function returns background co-accessibility scores for peaks of a given ArchRProject. The background is simulated by shuffling of features and cells.
+#'
+#' @param ArchRProj An `ArchRProject` object.
+#' @param reducedDims The name of the `reducedDims` object (i.e. "IterativeLSI") to retrieve from the designated `ArchRProject`.
+#' @param dimsToUse A vector containing the dimensions from the `reducedDims` object to use in clustering.
+#' @param scaleDims A boolean value that indicates whether to z-score the reduced dimensions for each cell. This is useful for minimizing
+#' the contribution of strong biases (dominating early PCs) and lowly abundant populations. However, this may lead to stronger sample-specific
+#' biases since it is over-weighting latent PCs. If set to `NULL` this will scale the dimensions based on the value of `scaleDims` when the
+#' `reducedDims` were originally created during dimensionality reduction. This idea was introduced by Timothy Stuart.
+#' @param corCutOff A numeric cutoff for the correlation of each dimension to the sequencing depth. If the dimension has a correlation to
+#' sequencing depth that is greater than the `corCutOff`, it will be excluded from analysis.
+#' @param cellsToUse A character vector of cellNames to compute coAccessibility on if desired to run on a subset of the total cells.
+#' @param k The number of k-nearest neighbors to use for creating single-cell groups for correlation analyses.
+#' @param knnIteration The number of k-nearest neighbor groupings to test for passing the supplied `overlapCutoff`.
+#' @param overlapCutoff The maximum allowable overlap between the current group and all previous groups to permit the current group be
+#' added to the group list during k-nearest neighbor calculations.
+#' @param maxDist The maximum allowable distance in basepairs between two peaks to consider for co-accessibility.
+#' @param scaleTo The total insertion counts from the designated group of single cells is summed across all relevant peak regions from
+#' the `peakSet` of the `ArchRProject` and normalized to the total depth provided by `scaleTo`.
+#' @param log2Norm A boolean value indicating whether to log2 transform the single-cell groups prior to computing co-accessibility correlations.
+#' @param seed A number to be used as the seed for random number generation required in knn determination. It is recommended to keep track
+#' of the seed used so that you can reproduce results downstream.
+#' @param threads The number of threads to be used for parallel computing.
+#' @param verbose A boolean value that determines whether standard output should be printed.
+#' @param logFile The path to a file to be used for logging ArchR output.
+#' @return List of feature- and observation-shuffled background co-accessibility scores.
+#' @keywords background co-accessibility archr rwire archrwire
+#' @examples Coming soon.
+#' @export
+
+
+getBackgroundCoAccessibility <- function(
+  ArchRProj = NULL,
+  reducedDims = "IterativeLSI",
+  dimsToUse = 1:30,
+  scaleDims = NULL,
+  corCutOff = 0.75,
+  cellsToUse = NULL,
+  cellAggregation = "duplicated", 
+  k = 100, 
+  knnIteration = 500, 
+  overlapCutoff = 0.8, 
+  maxDist = 100000,
+  scaleTo = 10^4,
+  log2Norm = TRUE,
+  seed = 1, 
+  threads = getArchRThreads(),
+  verbose = TRUE,
+  logFile = createLogFile("addBackgroundCoAccessibility")
+  ){
+
+  .validInput(input = ArchRProj, name = "ArchRProj", valid = c("ArchRProj"))
+  .validInput(input = reducedDims, name = "reducedDims", valid = c("character"))
+  .validInput(input = dimsToUse, name = "dimsToUse", valid = c("numeric", "null"))
+  .validInput(input = scaleDims, name = "scaleDims", valid = c("boolean", "null"))
+  .validInput(input = corCutOff, name = "corCutOff", valid = c("numeric", "null"))
+  .validInput(input = cellsToUse, name = "cellsToUse", valid = c("character", "null"))
+  .validInput(input = k, name = "k", valid = c("integer"))
+  .validInput(input = knnIteration, name = "knnIteration", valid = c("integer"))
+  .validInput(input = overlapCutoff, name = "overlapCutoff", valid = c("numeric"))
+  .validInput(input = maxDist, name = "maxDist", valid = c("integer"))
+  .validInput(input = scaleTo, name = "scaleTo", valid = c("numeric"))
+  .validInput(input = log2Norm, name = "log2Norm", valid = c("boolean"))
+  .validInput(input = threads, name = "threads", valid = c("integer"))
+  .validInput(input = verbose, name = "verbose", valid = c("boolean"))
+  .validInput(input = logFile, name = "logFile", valid = c("character"))
+
+  tstart <- Sys.time()
+  .startLogging(logFile = logFile)
+  .logThis(mget(names(formals()),sys.frame(sys.nframe())), "addCoAccessibility Input-Parameters", logFile = logFile)
+
+  set.seed(seed)
+
+  #Get Peak Set
+  peakSet <- getPeakSet(ArchRProj)
+
+  #Get Reduced Dims
+  rD <- getReducedDims(ArchRProj, reducedDims = reducedDims, corCutOff = corCutOff, dimsToUse = dimsToUse)
+  if(!is.null(cellsToUse)){
+    rD <- rD[cellsToUse, ,drop=FALSE]
+  }
+
+  ### Select most distant (randomly distributed) cells in low dimensional embedding for unique cell aggregation
+  if(cellAggregation == "unique"){
+      # From: https://stackoverflow.com/questions/22152482/choose-n-most-distant-points-in-r
+      bestavg <- 0
+      bestSet <- NA
+      for (i in 1:nrow(rD)){
+          subset <- rD[sample(1:nrow(rD),knnIteration),]
+          avg <- mean(dist(subset))
+          if (avg > bestavg) {
+              bestavg <- avg
+              bestSet <- subset
+          }
+      }
+      idx <- match(rownames(bestSet), rownames(rD))
+  } else if (cellAggregation == "duplicated"){
+      idx <- sample(seq_len(nrow(rD)), knnIteration, replace = !nrow(rD) >= 
+      knnIteration)
+  }
+  ###
+    
+  #KNN Matrix
+  .logDiffTime(main="Computing KNN", t1=tstart, verbose=verbose, logFile=logFile)
+  ### Select every cell only once for unique cell aggregation
+  if(cellAggregation == "unique"){
+      query1 <- rD[idx[1], ] %>% as.matrix(.) %>% t(.)
+      rownames(query1) <- rownames(rD)[1]
+      y <- .computeKNN(data = rD[-idx,], query = query1, k = k-1)
+      names <- rownames(rD[-idx,])[as.integer(y)]
+      knnObj <- c(idx[1], match(names, rownames(rD))) %>% as.matrix(.) %>% t(.)
+       
+      for (i in 2:knnIteration){
+          query1 <- rD[idx[i], ] %>% as.matrix(.) %>% t(.)
+          rownames(query1) <- rownames(rD)[i]
+          y <- .computeKNN(data = rD[-unique(c(idx, knnObj %>% as.integer())),], query = query1, k = k-1)
+          names <- rownames(rD[-unique(c(idx, knnObj %>% as.integer())),])[as.integer(y)]
+          knnObj <- rbind(knnObj, c(idx[i], match(names, rownames(rD))))
+      }
+  } else if (cellAggregation == "duplicated"){
+      knnObj <- .computeKNN(data = rD, query = rD[idx, ], k = k)
+  }
+  ###
+
+  #Determine Overlap
+  .logDiffTime(main="Identifying Non-Overlapping KNN pairs", t1=tstart, verbose=verbose, logFile=logFile)
+  keepKnn <- determineOverlapCpp(knnObj, floor(overlapCutoff * k))
+
+  #Keep Above Cutoff
+  knnObj <- knnObj[keepKnn==0,]
+  .logDiffTime(paste0("Identified ", nrow(knnObj), " Groupings!"), t1=tstart, verbose=verbose, logFile=logFile)
+
+  #Convert To Names List
+  ### convert integer to matrix:array
+  if (k == 1){
+      knnObj <- matrix(knnObj, nrow = knnIteration)
+  }
+  ###
+  knnObj <- lapply(seq_len(nrow(knnObj)), function(x){
+    rownames(rD)[knnObj[x, ]]
+  }) %>% SimpleList
+
+  #Check Chromosomes
+  chri <- gtools::mixedsort(.availableChr(getArrowFiles(ArchRProj), subGroup = "PeakMatrix"))
+  chrj <- gtools::mixedsort(unique(paste0(seqnames(getPeakSet(ArchRProj)))))
+  stopifnot(identical(chri,chrj))
+
+  #Create Ranges
+  peakSummits <- resize(peakSet, 1, "center")
+  peakWindows <- resize(peakSummits, maxDist, "center")
+
+  #Create Pairwise Things to Test
+  o <- DataFrame(findOverlaps(peakSummits, peakWindows, ignore.strand = TRUE))
+  o <- o[o[,1] != o[,2],]
+  o$seqnames <- seqnames(peakSet)[o[,1]]
+  o$idx1 <- peakSet$idx[o[,1]]
+  o$idx2 <- peakSet$idx[o[,2]]
+  o$correlation <- -999
+  o_featShuffle <- o
+  o_cellShuffle <- o
+
+  #Peak Matrix ColSums
+  cS <- .getColSums(getArrowFiles(ArchRProj), chri, verbose = FALSE, useMatrix = "PeakMatrix")
+  gS <- unlist(lapply(seq_along(knnObj), function(x) sum(cS[knnObj[[x]]], na.rm=TRUE)))
+  ### Add pseudo-count to gS for non-accessible cell aggregates in all regions of interest
+  gS <- gS + 1
+  ###
+
+  for(x in seq_along(chri)){
+  
+    .logDiffTime(sprintf("Computing Co-Accessibility %s (%s of %s)", chri[x], x, length(chri)), t1=tstart, verbose=verbose, logFile=logFile)
+
+    #Features
+    featureDF <- mcols(peakSet)[BiocGenerics::which(seqnames(peakSet) == chri[x]),]
+    featureDF$seqnames <- chri[x]
+
+    #Group Matrix
+    groupMat <- .getGroupMatrix(
+      ArrowFiles = getArrowFiles(ArchRProj), 
+      featureDF = featureDF, 
+      groupList = knnObj, 
+      useMatrix = "PeakMatrix",
+      threads = threads,
+      verbose = FALSE
+    )
+    
+    #Scale
+    groupMat <- t(t(groupMat) / gS) * scaleTo
+      
+    #Shuffle features for background correlations
+    groupMat_featShuffle <- apply(groupMat, 2, function(x){sample(x)}) ### shuffle features
+    groupMat_cellShuffle <- t(apply(groupMat, 1, function(x){sample(x)})) ### shuffle cells
+
+    if(log2Norm){
+      groupMat_featShuffle <- log2(groupMat_featShuffle + 1)
+      groupMat_cellShuffle <- log2(groupMat_cellShuffle + 1)
+    }
+
+    #Correlations
+    idx <- BiocGenerics::which(o$seqnames==chri[x])
+    corVals_featShuffle <- rowCorCpp(idxX = o[idx,]$idx1, idxY = o[idx,]$idx2, X = as.matrix(groupMat_featShuffle), Y = as.matrix(groupMat_featShuffle))
+    corVals_cellShuffle <- rowCorCpp(idxX = o[idx,]$idx1, idxY = o[idx,]$idx2, X = as.matrix(groupMat_cellShuffle), Y = as.matrix(groupMat_cellShuffle))
+    .logThis(head(corVals_featShuffle), paste0("SubsetCorVals-", x), logFile = logFile)
+    .logThis(head(corVals_cellShuffle), paste0("SubsetCorVals-", x), logFile = logFile)
+
+    o_featShuffle[idx,]$correlation <- as.numeric(corVals_featShuffle)
+    o_cellShuffle[idx,]$correlation <- as.numeric(corVals_cellShuffle)
+
+    .logThis(groupMat, paste0("SubsetGroupMat-", x), logFile = logFile)
+    .logThis(o[idx,], paste0("SubsetCoA-", x), logFile = logFile)
+
+  }
+  
+  mcols(peakSet) <- NULL
+
+  o_featShuffle$idx1 <- NULL
+  o_featShuffle$idx2 <- NULL
+  o_featShuffle <- o_featShuffle[!is.na(o_featShuffle$correlation),]
+  o_featShuffle@metadata$peakSet <- peakSet
+                      
+  o_cellShuffle$idx1 <- NULL
+  o_cellShuffle$idx2 <- NULL
+  o_cellShuffle <- o_cellShuffle[!is.na(o_cellShuffle$correlation),]
+  o_cellShuffle@metadata$peakSet <- peakSet
+  
+  .endLogging(logFile = logFile)
+
+  list(featShuffle=o_featShuffle, cellShuffle=o_cellShuffle)
+
+}
---
+++ R/plotCoAccessibilityMap.R
@@ -0,0 +1,95 @@
+#' plotCoAccessibilityMap
+#' 
+#' This function returns co-accessibility map of specified genomic region.
+#'
+#' @param ArchRProj An `ArchRProject` object.
+#' @param reducedDims The name of the `reducedDims` object (i.e. "IterativeLSI") to retrieve from the designated `ArchRProject`.
+#' @param region GRanges object of length 1 with region of interest for which co-accessibility should be calculated.
+#' @param resolution Tile size for which co-accessibility should be calculated. Defaults to 1 kb.
+#' @param ArchRGenome Used genome and gene annotation.
+#' @param
+#' @param aggregation_k The number of k-nearest neighbors to use for creating single-cell groups for correlation analyses (number of cells per aggregate). 
+#' @param aggregation_knnIteration The number of k-nearest neighbor groupings to test for passing the supplied overlapCutoff (number of aimed cell aggregates).
+#' @param aggregation_overlapCutoff The maximum allowable overlap between the current group and all previous groups to permit the current group be added to the group list during k-nearest neighbor calculations.
+#' @return Co-accessibility map, GRanges object of co-accessibility scores, and tile set of region.
+#' @keywords co-accessibility map coAccessibilityMap archr rwire archrwire
+#' @examples Coming soon.
+#' @export
+
+
+plotCoAccessibilityMap <- function(
+    ArchRProj = NULL,
+    reducedDims = names(ArchRProj@reducedDims)[1],
+    region = NULL,
+    resolution = 1000,
+    ArchRGenome = getArchRGenome(),
+    cor_max = NULL,
+    aggregation_k = 100,
+    aggregation_knnIteration = 500,
+    aggregation_overlapCutoff = 0.8
+    ){
+    
+    .validInput(input = ArchRProj, name = "ArchRProj", valid = c("ArchRProj"))
+    .validInput(input = reducedDims, name = "reducedDims", valid = c("character"))
+    .validInput(input = region, name = "region", valid = c("GRanges"))
+    .validInput(input = resolution, name = "resolution", valid = c("numeric"))
+    .validInput(input = ArchRGenome, name = "ArchRGenome", valid = c("character"))
+    
+    ### Create temporary ArchRProject
+    dir.create('tmp', showWarnings = FALSE)
+    ArchRProj <- saveArchRProject(ArchRProj = ArchRProj, outputDirectory = 'tmp/ArchRProj_plotCoAccessibilityMap', load = TRUE)
+    
+    ### Generate tile set
+    if (ArchRGenome == 'mm10'){ # base tile set on all chr (otherwise bug for chr16) 
+        tiles_base <- GRanges(seqnames=Rle(paste0("chr", c(1:19, "X")), 1), ranges=IRanges(rep(10000,20), end=rep(11000,20)))
+    } else if (ArchRGenome == 'Hg38'){
+        tiles_base <- GRanges(seqnames=Rle(paste0("chr", c(1:22, "X")), 1), ranges=IRanges(rep(10000,23), end=rep(11000,23)))
+    } else {
+        stop('Genome built not included yet.')
+    }
+
+    numTiles <- (end(region)-start(region))/resolution
+    tiles_region <- tile(region, n=numTiles)[[1]]
+    tiles <- c(tiles_base, tiles_region)
+
+    if (ArchRGenome == 'mm10'){ # GC content for addCoAccessibility function (does not work without GC content for normalization)
+        library(BSgenome.Mmusculus.UCSC.mm10)
+        tiles$GC <- Repitools::gcContentCalc(tiles, organism=Mmusculus)
+    } else if (ArchRGenome == 'Hg38'){
+        library(BSgenome.Hsapiens.UCSC.hg38)
+        tiles$GC <- Repitools::gcContentCalc(tiles, organism=Hsapiens)
+    }
+    
+    ### Calculate co-accessibility scores in region
+    ArchRProj <- addPeakSet(ArchRProj, peakSet = tiles, force=TRUE)
+    ArchRProj <- addPeakMatrix(ArchRProj)
+    
+    ArchRProj <- addCoAccessibility(ArchRProj, reducedDims = reducedDims, maxDist = 2*(end(region)-start(region)), k = aggregation_k, knnIteration = aggregation_knnIteration, overlapCutoff = aggregation_overlapCutoff)
+    coacc_tiles <- ArchRWire::getCoAccessibility(ArchRProj, corCutOff = 0, resolution = 1, returnLoops = TRUE)[[1]]
+      
+    ### Co-accessibility matrix visualization with RWire    
+    df_background <- expand.grid(tiles_region %>% gUtils::gr.mid(.) %>% start(.), tiles_region %>% gUtils::gr.mid(.) %>% start(.))
+    colnames(df_background) <- c('start', 'end')
+    df_background$correlation <- rep(0, nrow(df_background))
+    
+    df_coacc_tiles <- data.frame(start=start(coacc_tiles), end=end(coacc_tiles), correlation=coacc_tiles$value)
+    df_coacc_tiles <- rbind(df_coacc_tiles, df_background)
+    df_coacc_tiles <- df_coacc_tiles[!duplicated(df_coacc_tiles[, c('start', 'end')]),]
+    
+    mtx_coacc_tiles <- xtabs(correlation ~ start + end, data = df_coacc_tiles, sparse = TRUE)
+    
+    if (is.null(cor_max)){
+        cor_max <- max(df_coacc_tiles$correlation)
+    }
+
+    palette <- rgb(1, 1 - (0:255)/255, 1 - (0:255)/255)
+    
+    fields::image.plot(as.matrix(mtx_coacc_tiles), zlim=c(0, cor_max), axes = FALSE, main=as.character(region), col=palette)
+    abline(a=0, b=1, col='grey')
+    axis(side=1, at=0, labels=start(region), tick=FALSE)
+    axis(side=3, at=1, labels=end(tiles[length(tiles)]), tick=FALSE)
+    
+    p <- grDevices::recordPlot()
+    
+    return(list(CoAccessibilityMap = p, CoAccessibility = coacc_tiles, Tiles = tiles))
+}
